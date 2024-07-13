108af9d72dd2c82e7e4fce10e351ca9c18f7c9b8
adrian15 <adrian15sgd@gmail.com>
2024-07-13 12:24:50 +0200

    Add SELinux support
    Security-Enhanced Linux (SELinux) is a Linux kernel security module that provides a mechanism for supporting access control security policies, including mandatory access controls (MAC).
    This commit:
      - Adds extended attributes to the squashfs filesystem so that SELinux can be properly supported
      - Adds the needed SELinux packages so that SELinux can work in the squashfs Debian GNU/Linux system
      - Adds the proper kernel command line parametres to enable SELinux according to the chosen enforced or permissive mode.
    
    All of that combined let's you either:
      - Harden your live system ( `--selinux enforced` )
      - Enables you to interact with other SELinux filesystems without hardening the live system ( `--selinux permissive` ).
    
    - enforced mode means that the system will not allow a program/process to interact with a file with which it does not have the proper filesystem permission (based on SELinux extended attributes).
    - permissive mode means that the system will allow program/process to interact with a file with which it does not have the proper filesystem permission (based on SELinux extended attributes). This SELinux policy infrigenment will be logged though so that it can be properly fixed if needed.
    
    Default mode is `disable`.

--- functions/configuration.sh
+++ functions/configuration.sh
@@ -403,6 +403,30 @@ Prepare_config ()
 			;;
 	esac
 
+	LB_SELINUX="${LB_SELINUX:-disable}"
+
+	case "${LB_SELINUX}" in
+		enforced)
+			SELINUX_ENFORCED_CMDLINE="selinux=1 security=selinux enforcing=1"
+			if ! echo "${LB_BOOTAPPEND_LIVE}" | grep -q "${SELINUX_ENFORCED_CMDLINE}"
+			then
+				LB_BOOTAPPEND_LIVE="${LB_BOOTAPPEND_LIVE} ${SELINUX_ENFORCED_CMDLINE}"
+			fi
+		;;
+
+		permissive)
+			SELINUX_PERMISSIVE_CMDLINE="selinux=1 security=selinux enforcing=0"
+			if ! echo "${LB_BOOTAPPEND_LIVE}" | grep -q "${SELINUX_PERMISSIVE_CMDLINE}"
+			then
+				LB_BOOTAPPEND_LIVE="${LB_BOOTAPPEND_LIVE} ${SELINUX_PERMISSIVE_CMDLINE}"
+			fi
+		;;
+
+		disable)
+		;;
+
+	esac
+
 	local _LB_BOOTAPPEND_PRESEED
 	if [ -n "${LB_DEBIAN_INSTALLER_PRESEEDFILE}" ]
 	then
@@ -740,6 +764,11 @@ Validate_config_permitted_values ()
 		exit 1
 	fi
 
+	if ! In_list "${LB_SELINUX}" enforced permissive disable; then
+		Echo_error "You have specified an invalid value for LB_SELINUX (--selinux)."
+		exit 1
+	fi
+
 	if ! In_list "${LB_SOURCE_IMAGES}" iso netboot tar hdd; then
 		Echo_error "You have specified an invalid value for LB_SOURCE_IMAGES (--source-images)."
 		exit 1
@@ -800,6 +829,17 @@ Validate_config_dependencies ()
 			Echo_error "You have selected an invalid combination of bootloaders and live image type; the grub-* bootloaders are not compatible with hdd and netboot types."
 			exit 1
 		fi
+	fi
+
+	if In_list "${LB_SELINUX}" permissive enforced; then
+		if [ "${LB_CHROOT_FILESYSTEM}" != "squashfs" ]; then
+			Echo_error "You have selected values of LB_SELINUX and LB_CHROOT_FILESYSTEM which are incompatible. SELinux only supports squashfs as the chroot filesystem."
+			exit 1
+		fi
+	fi
+
+	if [ "${LB_SELINUX}" = "enforced" ]; then
+		Echo_warning "A value of 'enforced' for option LB_SELINUX is known not to boot onto an usable Live CD."
 	fi
 
 	Validate_http_proxy
