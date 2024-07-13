2c0de043de916efac85194c434df642525f143ed
adrian15 <adrian15sgd@gmail.com>
2024-07-13 20:22:46 +0200

    Add SELinux support (9)

--- functions/configuration.sh
+++ functions/configuration.sh
@@ -403,6 +403,35 @@ Prepare_config ()
 			;;
 	esac
 
+LB_SELINUX="${LB_SELINUX:-auto}"
+
+case "${LB_SELINUX}" in
+	enforced)
+		SELINUX_ENFORCED_CMDLINE="selinux=1 security=selinux enforcing=1"
+		if ! echo "${LB_BOOTAPPEND_LIVE}" | grep -q "${SELINUX_ENFORCED_CMDLINE}"
+		then
+			LB_BOOTAPPEND_LIVE="${LB_BOOTAPPEND_LIVE} ${SELINUX_ENFORCED_CMDLINE}"
+		fi
+	;;
+
+	permissive)
+		SELINUX_PERMISSIVE_CMDLINE="selinux=1 security=selinux enforcing=0"
+		if ! echo "${LB_BOOTAPPEND_LIVE}" | grep -q "${SELINUX_PERMISSIVE_CMDLINE}"
+		then
+			LB_BOOTAPPEND_LIVE="${LB_BOOTAPPEND_LIVE} ${SELINUX_PERMISSIVE_CMDLINE}"
+		fi
+	;;
+
+	#auto)
+	#
+	#;;
+	#
+	#disable)
+	#
+	#;;
+
+esac
+
 	local _LB_BOOTAPPEND_PRESEED
 	if [ -n "${LB_DEBIAN_INSTALLER_PRESEEDFILE}" ]
 	then
